71ca69694ee0b4afc6cece93e9ee2a8f2a7f514e
katitkatt <myEmail@example.com>
2025-10-30 11:09:00 +0300

    la

--- lab3/lib/main.dart
+++ lab3/lib/main.dart
@@ -7,9 +7,13 @@ void main  () async {
  
    runApp(MaterialApp(home:MyApp())); }
 
+class MyApp extends StatefulWidget{
+  @override
+  _MyAppState createState() => _MyAppState();
+}
 
 
-class MyApp extends StatelessWidget {
+class _MyAppState extends State<MyApp> {
     Future<List<dynamic>> _fetchData() async { 
 
      final res = await Supabase.instance.client
@@ -18,19 +22,93 @@ class MyApp extends StatelessWidget {
 
      return res; 
     }
+
+     Future<void> _addData() async {
+      try{
+    await Supabase.instance.client
+        .from('messages')
+        .insert({
+          'id': '28', 
+          'created_at': DateTime.now().toIso8601String(),
+          'text': '20:31 прибыл годжо сатору',
+          'username': 'kittika'
+        });
+         print(' Запись успешно добавлена в таблицу messages!');
+    
+    setState(() {});
+  } catch (e) {
+    print('Ошибка при добавлении записи: $e');
+  }
+  }
     
     @override 
     Widget build(BuildContext context) {
 
-    return Scaffold(body:Container(color:Colors.red,width:200,height:200,
-    
-    child: FutureBuilder<List<dynamic>>(
-            future: _fetchData(),  
-            builder: (context, snapshot) {
-             return Text('Data: ${snapshot.data}');
-            }
-        )
-      )
+    return Scaffold(body:
+    Column(children:[Container(color:Color.fromARGB(255, 174, 211, 224),width:800,height:600,
+    child: 
+     FutureBuilder<List<dynamic>>(
+              future: _fetchData(),
+              builder: (context, snapshot) { 
+     return ListView.builder(
+                  itemCount: snapshot.data!.length,
+                  itemBuilder: (context, i) {
+                    final item = snapshot.data![i];
+                    return Card(
+                        child: Column(                        
+                          children: [              
+                            Row(
+                              children: [
+                                Text(
+                                  'ID: ${item['id']}',
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    color: const Color.fromARGB(255, 163, 33, 243),
+                                  ),
+                                ),
+                                SizedBox(width: 16),
+                                Text(
+                                  'User: ${item['username']}',
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.green,
+                                  ),
+                                ),
+                              ],
+                            ),
+                            SizedBox(height: 8),
+                            
+                            
+                            Text(
+                              'Сообщение: ${item['text']}',
+                              style: TextStyle(fontSize: 16),
+                            ),
+                            SizedBox(height: 8),
+                            
+                            
+                            Text(
+                              'Дата: ${item['created_at']}',
+                              style: TextStyle(
+                                fontSize: 12,
+                                color: const Color.fromARGB(255, 71, 5, 102),
+                              ),
+                            ),
+                          ],
+                        ),
+                      
+                    );
+                  },
+                );
+              },
+            ),
+          ),
+       // Кнопка для добавления данных
+           ElevatedButton(
+              onPressed: _addData,
+              child: Text('Добавить запись'),
+            ),
+    ]
+    )
     );
   }
 }
